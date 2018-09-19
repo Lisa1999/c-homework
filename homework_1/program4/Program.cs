8f670bced99811ee83ed4236cd2fcbbbfcdddc6b
theoryxsy <2017302580167@whu.edu.com>
2018-09-19 19:35:55 +0800

    第二次作业

---
+++ homework_1/program4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入两个数字:");
+            string s1 = "";
+            string s2 = "";
+            double m = 0;
+            double n = 0;
+            s1 = Console.ReadLine();
+            s2 = Console.ReadLine();
+            m = Double.Parse(s1);
+            n = Double.Parse(s2);
+            Console.WriteLine("他们的乘积是：" + (m * n));
+        }
+    }
+}
---
+++ homework_2/program6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数：");
+            int m = Int32.Parse(Console.ReadLine());
+            int i = 2;
+            while (m > 2)
+            {
+                if (m % i == 0)
+                {
+                    m /= i;
+                    if (m >= 2)
+                    {
+                        Console.Write(i + "*");
+                    }
+                    else Console.Write(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
