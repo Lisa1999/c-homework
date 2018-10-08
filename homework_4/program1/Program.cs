d99e3b541f7ee26d977ed49d8da01a34dcb0297e
theoryxsy <2017302580167@whu.edu.com>
2018-10-08 19:01:47 +0800

    第四次提交

---
+++ homework_4/program1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    //1.声明参数类型
+    public class ClockAlarmEventArgs : EventArgs
+    {
+
+    }
+    //2.声明委托类型
+    public delegate void ClockAlarmEventHandler(object sender, ClockAlarmEventArgs e);
+
+    //定义事件源(闹表)
+    public class ClockAlarm
+    {
+        //3.声明事件 
+        public event ClockAlarmEventHandler Clocking; //在该类中对事件进行调用
+        public void DoClock()
+        {
+            //4.发生一个事件,通知外界
+            if (Clocking != null)
+            {
+                ClockAlarmEventArgs args = new ClockAlarmEventArgs();
+                Clocking(this, args);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入所需闹钟时间小时数: ");
+            string h = Console.ReadLine();
+            try
+            {
+                while (Int32.Parse(h) > 23 || Int32.Parse(h) < 0)
+                {
+                    Console.WriteLine("输入不合理,请重新输入:");
+                    h = Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输入不合理,请重新输入:");
+            }
+            Console.WriteLine("请输入所需闹钟时间分钟数: ");
+            string m = Console.ReadLine();
+            try
+            {
+                if (m.Length == 1) m = "0" + m;
+                while (Int32.Parse(m) > 59 || Int32.Parse(m) < 0)
+                {
+                    Console.WriteLine("输入不合理,请重新输入: ");
+                    m = Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("输入不合理,请重新输入: ");
+            }
+            string s = h + ":" + m;
+            //注册一个闹钟
+            var clockAlarm = new ClockAlarm();
+            string nowTime = DateTime.Now.ToShortTimeString().ToString();
+            Console.WriteLine("现在是: " + nowTime);
+            while (nowTime != s)
+            {
+                //逐渐求当前时间以延迟一分钟代替
+                System.Threading.Thread.Sleep(60000);
+                nowTime = DateTime.Now.ToShortTimeString().ToString();
+                Console.WriteLine("现在是: " + nowTime);
+            }
+            //5.注册事件
+            clockAlarm.Clocking += Ring;
+            clockAlarm.DoClock();
+        }
+        //6.事件处理方法
+        static void Ring(object sender, ClockAlarmEventArgs e)
+        {
+            Console.WriteLine("---------闹钟时刻到了，响铃啦响铃啦--------");
+        }
+
+    }
+}
---
+++ homework_4/program2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order myorder = new Order();
+            Program pro = new Program();
+            //初始化订单
+            OrderDetail od1 = new OrderDetail("老人与海", "001", "gyh", "10", "24");
+            OrderDetail od2 = new OrderDetail("围城", "002", "csy", "5", "56");
+            OrderDetail od3 = new OrderDetail("读者", "003", "gyt", "4", "37");
+            //添加订单
+            pro.addOrder(myorder, od1);
+            pro.addOrder(myorder, od2);
+            pro.addOrder(myorder, od3);
+            //删除订单
+            pro.deleteOrder(myorder, od1);
+            int index = pro.findIndex(myorder, "读者");
+            //修改订单
+            pro.changeOrder(myorder, index, 0, "格林童话");
+            //打印订单
+            pro.printOrder(myorder);
+
+
+        }
+        //添加订单
+        public void addOrder(Order order, OrderDetail od)
+        {
+            order.OrderList.Add(od);
+        }
+        //删除订单
+        public void deleteOrder(Order order, OrderDetail od)
+        {
+            try
+            {
+                order.OrderList.Remove(od);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("订单为空无法删除!");
+            }
+
+        }
+        //修改订单
+        public void changeOrder(Order order, int index, int key, string s)
+        {
+            try
+            {
+                order.OrderList[index].myDictionary[key] = s;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("键值不合理!");
+            }
+        }
+        public int findIndex(Order order, string s)
+        {
+            for (int i = 0; i < order.OrderList.Count; i++)
+            {
+                if (order.OrderList[i].myDictionary[0] == s || order.OrderList[i].myDictionary[1] == s || order.OrderList[i].myDictionary[2] == s)
+                    return i;
+            }
+            return -1;
+
+        }
+        public void printOrder(Order order)
+        {
+            foreach (OrderDetail od in order.OrderList)
+            {
+                Console.WriteLine(od.myDictionary[0] + "" + od.myDictionary[1] + " " + od.myDictionary[2] + " " + od.myDictionary[3] + " " + od.myDictionary[4]);
+            }
+        }
+
+    }
+    class Order
+    {
+        public List<OrderDetail> OrderList = new List<OrderDetail>();//订单列表
+    }
+    class OrderDetail
+    {
+        public Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+        public OrderDetail(string name = "", string id = "", string clientName = "", string num = "", string price = "")
+        {
+            myDictionary[0] = name;          //商品名称
+            myDictionary[1] = id;            //商品订单号
+            myDictionary[2] = clientName;    //客户名称
+            myDictionary[3] = num;           //订购数量
+            myDictionary[4] = price;         //商品价格
+        }
+        public OrderDetail ShallowCopy()
+        {
+            return (OrderDetail)this.MemberwiseClone();
+        }
+    }
+}
